88f351770ea82b80954235d1f3992556bd2743d0
ryun <run2070@naver.com>
2021-02-20 13:15:28 +0900

    디펜던시 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,23 +21,35 @@ repositories {
 
 
 dependencies {
+
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	//mybatis dependencies
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.1.4")
+	//postgresql
+	implementation("org.postgresql:postgresql:42.2.18")
+	//swagger dependencies
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+	implementation("io.springfox:springfox-swagger2:2.9.2")
+	//사용하는 dependencies
+
+
 	//implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.springframework.integration:spring-integration-http")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("com.googlecode.log4jdbc:log4jdbc:1.2")
-	//mybatis dependencies
-	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.1.4")
-	//postgresql
-	implementation("org.postgresql:postgresql:42.2.18")
+
+	implementation("android.arch.persistence.room:runtime:1.1.1")
+	annotationProcessor("android.arch.persistence.room:compiler:1.1.1")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
+
+
 	// For markdown convert ( From atlassian(jira/confluence) library )
 	implementation("com.atlassian.commonmark:commonmark:0.11.0")
 	implementation("com.atlassian.commonmark:commonmark-ext-autolink:0.11.0")
