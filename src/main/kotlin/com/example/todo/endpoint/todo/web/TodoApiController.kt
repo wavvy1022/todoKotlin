8ade65ced90f1a5988ee50a5057bcdbe9020741f
ryun <run2070@naver.com>
2021-02-20 18:23:32 +0900

    업데이트 삭제 api url 수정

--- src/main/kotlin/com/example/todo/endpoint/todo/web/TodoApiController.kt
+++ src/main/kotlin/com/example/todo/endpoint/todo/web/TodoApiController.kt
@@ -4,12 +4,9 @@ import com.example.todo.endpoint.todo.domain.model.TodoDto
 import com.example.todo.endpoint.todo.domain.service.TodoService
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
-import org.springframework.http.HttpRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletRequest
-import javax.validation.Valid
-import javax.websocket.server.PathParam
 
 @Api(description = "Todo list 관리")
 @CrossOrigin("http://172.30.1.26:3000", allowCredentials = "true")
@@ -36,7 +33,7 @@ class TodoApiController(
     }
 
     @ApiOperation(value = "todo 수정", notes = "todo 수정 PUT API")
-    @PutMapping("/todo/index/{index}")
+    @PutMapping("/todo/index}")
     fun updateTodo(@RequestBody todoDto: TodoDto, @PathVariable index: Int): ResponseEntity<Any>{
         todoDto.index = index
         return if(todoService.updateTodo(todoDto)==1){
@@ -48,7 +45,7 @@ class TodoApiController(
     }
 
     @ApiOperation(value = "todo 삭제", notes = "todo 삭제 DELETE API")
-    @DeleteMapping("/todo/index/{index}")
+    @DeleteMapping("/todo/{index}")
     fun deleteToto(@PathVariable index:Int): ResponseEntity<Unit>{
         todoService.deleteTodo(index)
         return if(todoService.deleteTodo(index)==1){
