722b5f2c33c7712afdebc7b524fff20fe5f85143
shining <shinebless919@gmail.com>
2021-02-20 14:15:44 +0900

    * Delete not used dependencies * Refactor TodoApplication.kt

--- src/main/kotlin/com/example/todo/TodoApplication.kt
+++ src/main/kotlin/com/example/todo/TodoApplication.kt
@@ -3,10 +3,10 @@ package com.example.todo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-
 @SpringBootApplication
-class TodoApplication{
-    fun main(args: Array<String>) {
-        runApplication<TodoApplication>(*args)
-    }
+class TodoApplication
+
+fun main(args: Array<String>) {
+    runApplication<TodoApplication>(*args)
 }
+
