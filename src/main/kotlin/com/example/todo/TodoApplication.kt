3a037be8072dba160db160010f551529ea1dcb1f
shining <shinebless919@gmail.com>
2021-02-20 13:22:02 +0900

    * delete blog reference codes * Recreate TodoApplication.kt

---
+++ src/main/kotlin/com/example/todo/TodoApplication.kt
@@ -0,0 +1,12 @@
+package com.example.todo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class TodoApplication{
+    fun main(args: Array<String>) {
+        runApplication<TodoApplication>(*args)
+    }
+}
